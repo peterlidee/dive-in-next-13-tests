a155f64816e408509608265fe8a8bf19cf41cc17
peterlidee <peter@lidee.be>
2023-06-24 11:54:55 +0200

    Added tests

---
+++ app/test2/staticClient/page.js
@@ -0,0 +1,6 @@
+'use client';
+
+export default function StaticClient() {
+  console.log('Test 2: rendering StaticClient');
+  return <div>Client component in a static route</div>;
+}
---
+++ app/test2/staticServer/page.js
@@ -0,0 +1,4 @@
+export default function StaticServer() {
+  console.log('Test 2: rendering StaticServer');
+  return <div>Server component in a static route</div>;
+}
