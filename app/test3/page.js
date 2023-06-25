1778377851c56bff908a011fdc015dba621f893d
peterlidee <peter@lidee.be>
2023-06-25 22:21:00 +0200

    Updated and ordered links

--- app/test3/page.js
+++ app/test3/page.js
@@ -4,15 +4,11 @@ export default function Test3() {
   return (
     <div>
       <h1>Test 3</h1>
+      <h3>Static</h3>
       <ul>
         <li>
           <Link href='test3/static/clientInServer'>
             client in server / static route
-          </Link>
-        </li>
-        <li>
-          <Link href='test3/dynamic/clientInServer'>
-            client in server / dynamic route
           </Link>
         </li>
         <li>
@@ -21,17 +17,25 @@ export default function Test3() {
           </Link>
         </li>
         <li>
+          <Link href='test3/static/serverInClientAsChildren'>
+            server in client as children / static route
+          </Link>
+        </li>
+      </ul>
+      <h3>Dynamic</h3>
+      <ul>
+        <li>
+          <Link href='test3/dynamic/clientInServer'>
+            client in server / dynamic route
+          </Link>
+        </li>
+        <li>
           <Link href='test3/dynamic/serverInClient'>
             server in client / dynamic route
           </Link>
         </li>
         <li>
-          <Link href='test3/static/serverInClientAsChildren'>
-            server in client as children / static route
-          </Link>
-        </li>
-        <li>
-          <Link href='test3/dynamic/serverinclientaschildren'>
+          <Link href='test3/dynamic/serverInClientAsChildren'>
             server in client as children / dynamic route
           </Link>
         </li>
