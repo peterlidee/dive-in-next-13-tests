6b255fed55530e19242c36eacad7d76912ed2d1e
peterlidee <peter@lidee.be>
2023-06-26 18:44:41 +0200

    updated some components

--- components/ComponentWithState.js
+++ components/ComponentWithState.js
@@ -3,8 +3,11 @@ import { useState } from 'react';
 export default function ComponentWithState() {
   const [value, setValue] = useState('Peter');
   return (
-    <label>
-      name: <input value={value} onChange={(e) => setValue(e.target.value)} />
-    </label>
+    <div>
+      <h1>Component with state</h1>
+      <label>
+        name: <input value={value} onChange={(e) => setValue(e.target.value)} />
+      </label>
+    </div>
   );
 }
--- pages/test1/index.js
+++ pages/test1/index.js
@@ -1,12 +1,26 @@
-import Hello from '@/components/Hello';
-import Input from '@/components/ComponentWithState';
+import Link from 'next/link';
 
 export default function Test1() {
   return (
     <div>
       <h1>Test 1</h1>
-      <Hello />
-      <Input />
+      <ul>
+        <li>
+          <Link href='/test1/static'>Component 1</Link>
+        </li>
+        <li>
+          <Link href='/test1/staticWithState'>Component with state</Link>
+        </li>
+        <li>
+          <Link href='/test1/ssg'>Component SSG</Link>
+        </li>
+        <li>
+          <Link href='/test1/ssr'>Component SSR</Link>
+        </li>
+      </ul>
+      <div>
+        <Link href='/'>Back to home</Link>
+      </div>
     </div>
   );
 }
