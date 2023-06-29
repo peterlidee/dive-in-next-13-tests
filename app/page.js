aa9426ce42499fb1c0b0e932886e24dca4be65ef
peterlidee <peter@lidee.be>
2023-06-29 18:28:20 +0200

    Cleaned out page in root

--- app/page.js
+++ app/page.js
@@ -1,23 +1,7 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <div>
       <h1>home</h1>
-      <ul>
-        <li>
-          <Link href='/test1'>Test 1 (pages router)</Link>
-        </li>
-        <li>
-          <Link href='/test2'>Test 2</Link>
-        </li>
-        <li>
-          <Link href='/test3'>Test 3</Link>
-        </li>
-        <li>
-          <Link href='/test4'>Test 4</Link>
-        </li>
-      </ul>
     </div>
   );
 }
