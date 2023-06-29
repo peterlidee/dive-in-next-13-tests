ecb50b5915236bd9a5a2123b2cd555a09dd0bd8e
peterlidee <peter@lidee.be>
2023-06-29 17:53:56 +0200

    updated layout with links to all tests + removed old layouts

--- app/layout.js
+++ app/layout.js
@@ -9,9 +9,23 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <body>
         {children}
-        <div>
-          <Link href='/'>Back to: Home</Link>
-        </div>
+        <ul>
+          <li>
+            <Link href='/'>home</Link>
+          </li>
+          <li>
+            <Link href='/test1'>/test1</Link>
+          </li>
+          <li>
+            <Link href='/test2'>/test2</Link>
+          </li>
+          <li>
+            <Link href='/test3'>/test3</Link>
+          </li>
+          <li>
+            <Link href='/test4'>/test4</Link>
+          </li>
+        </ul>
       </body>
     </html>
   );
