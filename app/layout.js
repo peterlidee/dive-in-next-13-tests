04ad8b2514669897b6774a361d63a4a2892c073f
peterlidee <peter@lidee.be>
2023-06-29 18:29:46 +0200

    Added nav title

--- app/layout.js
+++ app/layout.js
@@ -9,23 +9,26 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <body>
         {children}
-        <ul>
-          <li>
-            <Link href='/'>home</Link>
-          </li>
-          <li>
-            <Link href='/test1'>/test1</Link>
-          </li>
-          <li>
-            <Link href='/test2'>/test2</Link>
-          </li>
-          <li>
-            <Link href='/test3'>/test3</Link>
-          </li>
-          <li>
-            <Link href='/test4'>/test4</Link>
-          </li>
-        </ul>
+        <nav>
+          <h3>Nav</h3>
+          <ul>
+            <li>
+              <Link href='/'>home</Link>
+            </li>
+            <li>
+              <Link href='/test1'>/test1</Link>
+            </li>
+            <li>
+              <Link href='/test2'>/test2</Link>
+            </li>
+            <li>
+              <Link href='/test3'>/test3</Link>
+            </li>
+            <li>
+              <Link href='/test4'>/test4</Link>
+            </li>
+          </ul>
+        </nav>
       </body>
     </html>
   );
