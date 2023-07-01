f69a11566c202da2a18f51938887809007ca9474
peterlidee <peter@lidee.be>
2023-07-01 14:49:01 +0200

    Removed title meta

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,5 @@
 import Link from 'next/link';
 
-export const metadata = {
-  title: 'TODO Client server static dynamic rendering tests',
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
